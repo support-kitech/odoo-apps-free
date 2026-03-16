84977a2d7b5306109a621797a44f1b0b8efb0882
Support Kitech <support@khichdiinfotech.com>
2026-03-16 13:02:04 +0530

    [UP] Updated template for purchase

--- ki_website_google_tag_manager/__init__.py
+++ ki_website_google_tag_manager/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
+# Copyright © 2025 Khichdi InfoTech (https://khichdiinfotech.com)
+
 from . import models
--- ki_website_google_tag_manager/models/website.py
+++ ki_website_google_tag_manager/models/website.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Khichdi Infotech
-
+# Copyright © 2025 Khichdi InfoTech (https://khichdiinfotech.com)
 from odoo import api, fields, models
 
 
